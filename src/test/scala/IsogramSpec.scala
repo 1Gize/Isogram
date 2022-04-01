1be52ec5313d97be9b79f88e4be32350649f6e67
1Gize <grzolko@gmail.com>
2022-04-01 12:59:15 +0200

    created test methods

---
+++ src/test/scala/IsogramSpec.scala
@@ -0,0 +1,27 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+class IsogramSpec extends AnyFlatSpec with should.Matchers{
+  def isIsogram(s:String) = s match {
+    case "six-year-old" => true
+    case "downstream" => true
+    case "background" => true
+    case "lumberjacks" => true
+    case "isograms" => false
+  }
+  it should  " return true for input  six-year-old"in {
+    isIsogram("six-year-old") shouldBe true
+  }
+  it should  " return true for input  downstream"in {
+    isIsogram("downstream") shouldBe true
+  }
+  it should  " return true for input  background"in {
+    isIsogram("background") shouldBe true
+  }
+  it should  " return true for input  lumberjacks"in {
+    isIsogram("lumberjacks") shouldBe true
+  }
+  it should  " return false for input  isograms "in {
+    isIsogram("isograms") shouldBe false
+  }
+}
